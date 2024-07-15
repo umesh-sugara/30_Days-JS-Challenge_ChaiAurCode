40a866862d86b9f82f03252402ab2daee2bf806d
Umesh Sugara <73294581+umesh-sugara@users.noreply.github.com>
2024-07-15 12:05:15 +0530

    Day1 Js Challenge Solution
    Task Link : https://courses.chaicode.com/learn/home/30-days-of-Javascript-challenge/30-days-javascript-challenge/section/515627/lesson/3196994

---
+++ Day1.js
@@ -0,0 +1,23 @@
+var Name = "Umesh"
+let name = "Umesh";
+console.log(Name, name);
+
+const mybool = true
+console.log(mybool)
+
+
+let num = 1
+let str = "mystring"
+let myboolean = false
+let arr = [1,"Umesh"]
+let obj = {name: "Umesh"}
+console.log(typeof(num), typeof(str), typeof(arr), typeof(obj), typeof(mybool))
+
+name = 'explorer';
+var Name = 'explorer'
+console.log(Name,name)
+
+
+// mybool = false
+// console.log(mybool)
+
