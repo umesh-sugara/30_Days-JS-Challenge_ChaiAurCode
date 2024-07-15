aef6e211e7fe5177a4b300934b01df2372eaec2d
Umesh Sugara <73294581+umesh-sugara@users.noreply.github.com>
2024-07-15 12:18:29 +0530

    Day2 Solution
    https://courses.chaicode.com/learn/home/30-days-of-Javascript-challenge/30-days-javascript-challenge/section/515627/lesson/3197073

---
+++ Day2.js
@@ -0,0 +1,14 @@
+let first = 2
+let second = 10
+
+console.log(first+second, first-second,first*second, second/first,second%first)
+
+console.log(first+=second, first-=second)
+
+console.log(first>second, first<second, first>=second, second<=first)
+console.log( first==second, first===second)
+
+console.log(first, second,first && second, first|| second, first^second, ! first)
+
+console.log( second<first ? "First is big": "Second is big")
+
